88843d72f9dd5f106f549c703b056bb6b30cae51
vilcinskij <vilcinskij@gmail.com>
2022-10-28 01:15:54 +0300

    task4-end

--- user.js
+++ user.js
@@ -1,24 +1,32 @@
 const userId = 9;
 
-let userCard = document.querySelector('#user-card');
-let postsWrapper = document.querySelector('#posts-wrapper');
-let postsHeader = document.createElement('h4')
+const userCard = document.querySelector('#user-card');
+const postsWrapper = document.querySelector('#posts-wrapper');
+const albumsWrapper = document.querySelector('#albums-wrapper');
+
+const postsHeader = document.createElement('h4')
+const albumHeader = document.createElement('h4')
+const postList = document.createElement('ul');
+
 
 postsHeader.textContent = 'User posts'
+albumHeader.textContent = 'User albums'
+
 postsWrapper.prepend(postsHeader);
+albumsWrapper.prepend(albumHeader);
 
-fetch(`https://jsonplaceholder.typicode.com/users/${userId}?_embed=posts`)
-   .then(res => res.json())
-   .then(data => {
-      // console.log(data);
-      let name = document.createElement('span');
-      let username = document.createElement('span');
-      let address = document.createElement('span');
-      let email = document.createElement('span');
-      let phone = document.createElement('span');
-      let website = document.createElement('span');
-      let company = document.createElement('span');
-
+fetch(`https://jsonplaceholder.typicode.com/users/${userId}?_embed=posts&_embed=albums`)
+.then(res => res.json())
+.then(data => {
+   console.log(data);
+   const name = document.createElement('span');
+      const username = document.createElement('span');
+      const address = document.createElement('span');
+      const email = document.createElement('span');
+      const phone = document.createElement('span');
+      const website = document.createElement('span');
+      const company = document.createElement('span');
+      
       name.textContent = `${data.name}`;
       username.textContent = `Username: ${data.username}`;
       address.textContent = `Address: ${data.name}`;
@@ -26,21 +34,37 @@ fetch(`https://jsonplaceholder.typicode.com/users/${userId}?_embed=posts`)
       phone.textContent = `Phone: ${data.phone}`;
       website.textContent = `Website: ${data.website}`;
       company.textContent = `Company: ${data.company.name}`;
-
+      
       data.posts.map(post => {
-         let postItem = document.createElement('div');
-         let postTitle = document.createElement('a');
-         let postContent = document.createElement('p');
-
+         const postItem = document.createElement('div');
+         const postTitle = document.createElement('a');
+         const postContent = document.createElement('p');
+         
          postTitle.href = '#'
          postTitle.textContent = post.title;
          postContent.textContent = post.body;
-
+         
          postItem.append(postTitle, postContent);
          postsWrapper.append(postItem);
-
+         
          postItem.classList.add('card', 'mt-4', 'p-3')
       })
+      
+      
+      data.albums.map(post => {
+         const postItem = document.createElement('li');
+         const postTitle = document.createElement('a');
+         
+         postTitle.href = './album.html'
+         postTitle.textContent = post.title;
+         
+         postItem.append(postTitle);
+         postList.append(postItem);
+         albumsWrapper.append(postList);
+         
+         // postItem.classList.add('card', 'mt-4', 'p-3')
+      })
+
 
       userCard.append(name, username, address, email, phone, website, company)
 
@@ -48,8 +72,4 @@ fetch(`https://jsonplaceholder.typicode.com/users/${userId}?_embed=posts`)
    })
 
 
-fetch(`https://jsonplaceholder.typicode.com/users/${userId}/albums`)
-   .then(res => res.json())
-   .then(albums => { 
-      const albumsWrapper = document.querySelector
-   })
+
